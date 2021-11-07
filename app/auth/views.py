02df35eeb06c55885980e0f1812dc5155482fe5e
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-11-07 13:19:03 +0300

    THis commit adds authentificcation for loging in and registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,5 +18,26 @@ def login():
 
         flash('Invalid username or Password')
     form = LoginForm()
-    title = "pitch perfect login"
+    title = "Elevator pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Elevator pitch","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/auth/logout')
+@login_required
+def logout():
+    logout_user()
+    
+    return redirect(url_for("main.index"))    
