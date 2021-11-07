550166d6a7722721a2461735940e5bff3f30b7b4
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-11-07 13:15:46 +0300

    This commit adds a routing for login form

---
+++ app/auth/views.py
@@ -0,0 +1,22 @@
+from .. import db
+from flask import render_template,redirect,url_for,flash,request
+from . import auth
+from ..models import User  
+from flask_login import login_user, logout_user,  login_required
+from .forms import RegistrationForm, LoginForm
+from ..email import mail_message
+
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+    form = LoginForm()
+    title = "pitch perfect login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
