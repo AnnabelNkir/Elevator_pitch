b21a3257c297b37a463baf0eacac56102ac308b7
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-11-07 18:32:49 +0300

    This commit adds configurations in the config.py file

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config:
+    SECRET_KEY='MhyKBOi5RplMO5bQWLkezQ'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_ONYX_URL')
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://annabel:codelife@localhost/elevatorpitch'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}    
