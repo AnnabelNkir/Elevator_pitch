8440f804977a2576c1e81a385a48fb6e3542de2e
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-11-10 16:15:02 +0300

    Deployment to Heroku

--- config.py
+++ config.py
@@ -23,7 +23,9 @@ class Config:
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    pass
 class TestConfig(Config):
     pass
 
